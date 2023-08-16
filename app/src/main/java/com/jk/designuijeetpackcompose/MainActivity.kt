00ab416d43df057d4ad2543abcb40642989a12b9
jakin6 <irijakin@gmail.com>
2023-08-16 16:48:24 +0200

    initial commit

--- app/src/main/java/com/jk/designuijeetpackcompose/MainActivity.kt
+++ app/src/main/java/com/jk/designuijeetpackcompose/MainActivity.kt
@@ -3,13 +3,25 @@ package com.jk.designuijeetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.jk.designuijeetpackcompose.ui.theme.DesignUiJeetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Jakin")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +42,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Greeting() {
+    val fontFamily= FontFamily(
+        Font(R.font.lexend_thin, FontWeight.Thin),
+        Font(R.font.lexend_light, FontWeight.Light) ,
+        Font(R.font.lexend_regular, FontWeight.Normal),
+        Font(R.font.lexend_medium, FontWeight.Medium),
+        Font(R.font.lexend_semibold, FontWeight.SemiBold),
+        Font(R.font.lexend_bold, FontWeight.Bold),
+        Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
     )
+Box(modifier= Modifier
+    .fillMaxSize()
+    .background(
+        Color.Magenta
+    )) {
+    Text(
+
+        text = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                color = Color.Green,
+                fontSize = 30.sp
+            ))
+                {
+                append("J")
+            }
+            append("etpack")
+            withStyle(
+                style = SpanStyle(
+                    color = Color.Green,
+                    fontSize = 30.sp
+                ))
+            {
+                    append("C")
+            }
+            append("ompose")
+        },
+        color = Color.White,
+        fontSize = 30.sp,
+        fontFamily = fontFamily ,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        textDecoration = TextDecoration.Underline
+   )
+}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DesignUiJeetpackComposeTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
