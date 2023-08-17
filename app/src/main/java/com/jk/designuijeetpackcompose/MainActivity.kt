81b70def7931fbb9198faad0e2ff312631b254cf
jakin6 <irijakin@gmail.com>
2023-08-17 08:44:55 +0200

    initial commit

--- app/src/main/java/com/jk/designuijeetpackcompose/MainActivity.kt
+++ app/src/main/java/com/jk/designuijeetpackcompose/MainActivity.kt
@@ -3,13 +3,24 @@ package com.jk.designuijeetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jk.designuijeetpackcompose.ui.theme.DesignUiJeetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Jakin")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +41,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Greeting() {
+//    val scrollState= rememberScrollState()
+    LazyColumn {
+        itemsIndexed(
+            listOf("This", "is", "Jetpack", "Compose")
+        ) {index,string->
+                Text(
+                    text = string,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 24.dp)
+                )
+            }
+//        for (i in 1..50){
+//            Text(
+//                text = "Item $i",
+//                fontSize = 24.sp,
+//                fontWeight = FontWeight.Bold,
+//                textAlign = TextAlign.Center,
+//                modifier=Modifier
+//                    .fillMaxWidth()
+//                    .padding(vertical = 24.dp)
+//            )
+//        }
+        }
+    }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DesignUiJeetpackComposeTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
